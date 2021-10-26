9bda7b679f53fc383a6d7e528ade21f9d9a364f1
Joseph DiMino <josephdimino05@gmail.com>
2021-10-26 18:35:41 -0400

    Fixing weird postcss bug

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 
+const port = 3000;
 
 var app = express();
 
@@ -19,7 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
 //routes ----------------------------------
 app.use('/', indexRouter);
 
