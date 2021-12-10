24b42b5099306bde589c3bf6739c6e98f59b93fb
Joseph DiMino <josephdimino05@gmail.com>
2021-12-10 08:13:33 -0500

    Cube

--- app.js
+++ app.js
@@ -10,17 +10,19 @@ var indexRouter = require('./routes/index');
 var animationRouter = require('./routes/animation');
 //server stuff
 const port = 3000;
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 //production things
 app.use(helmet());
 app.use(compression());
--- routes/animation.js
+++ routes/animation.js
@@ -1,9 +1,10 @@
+//express app studd
 var express = require('express');
 var router = express.Router();
 
 //get the animation page
 router.get('/', function(req, res, next) {
-    res.render('animation', { title: 'Animation' });
+    res.render('animation', { title: 'Animations' });
 });
-
+//export the router
 module.exports = router;
